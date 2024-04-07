f0d35544beffa317115f3cf3d766a1845e5ae71a
Washington Antonio <was.antonio.xy@gmail.com>
2024-04-07 17:18:32 -0400

    feat: add new font

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import Main from '../components/layouts/main'
-import theme from '../libs/theme';
+import Fonts from '../components/fonts'
+import theme from '../libs/theme.js';
 
 const Website = ({ Component, pageProps, router }) => {
     return (
         <ChakraProvider theme={theme}>
+            <Fonts />
             <Main router={router}>
                 <Component {...pageProps} key={router.key} />
             </Main>
