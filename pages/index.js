fa3d9e4f925b5da3e1f6cec8cc3bbfcd77453f3e
Washington <was.antonio.xy@gmail.com>
2025-10-30 17:32:59 -0400

    style: creating highlight box to skills and organizing bio

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,8 @@ import {
     Text,
     Grid,
     GridItem,
-    Icon
+    Icon,
+    Badge
 } from '@chakra-ui/react'
 import NextLink from 'next/link'
 import Section from '../components/section'
@@ -23,6 +24,15 @@ import SocialMediaData from '../constants/social-media-data'
 import TECH_SKILLS_DATA from '../constants/tech-skills-data'
 
 const Page = () => {
+
+    const HighlightBox = ({ children }) => {
+        return (
+            <Badge display={'inline'} colorScheme='green'>
+                {children}
+            </Badge>
+        );
+    }
+
     return (
         <Layout>
             <Container mt={'3rem'} >
@@ -46,18 +56,26 @@ const Page = () => {
                                 <Heading as='h2' variant='page-title'>
                                     Washington Antonio
                                 </Heading>
-                                <Text color={'gray'}>Software Engineer • 3 years of experience • Web developer • Full Stack </Text>
+                                <Text color={'gray'}>Software Engineer • 3+ years of experience • Web developer • Full Stack • IT Technician</Text>
                             </Box>
 
                             <Section delay={0.3}>
                                 <Heading as='h3' variant="section-title">
-                                    About me
+                                    😎 About me
                                 </Heading>
                                 <Paragraph>
-                                    I am Computer Science student at <Link href='https://ufam.edu.br/'>Universidade Federal do Amazonas
-                                    </Link> and currently working at <Link href='https://www.itriad.org.br/'>Itriad Systems</Link>. I have 2 years of experience developing websites and mobile apps.
-                                    I also have a technical course certificate from <Link href='https://www.fundacaomatiasmachline.org.br/'>Fundação Matias Machline</Link> where I did my graduation from high school and learned about computers.
+                                    I’m a Software Developer with 3+ years of experience building real-world applications using <HighlightBox>ReactJS</HighlightBox>, <HighlightBox>Next.js</HighlightBox> <HighlightBox>Node.js</HighlightBox>, <HighlightBox>TypeScript</HighlightBox> <HighlightBox>JavaScript</HighlightBox> , <HighlightBox>Express.js</HighlightBox>, <HighlightBox>MongoDB</HighlightBox>, and <HighlightBox>PostgreSQL</HighlightBox>.
+                                    I’m passionate about writing clean, scalable, and maintainable code following modern best practices. <br /><br />
+
+                                    I have worked in agile environments (<strong>Scrum</strong>, <strong>Kanban</strong>), collaborating with cross-functional teams to deliver efficient, user-friendly solutions.
+                                    As a freelancer, I’ve focused on front-end projects and enjoy building MVPs that combine creativity, functionality, and entrepreneurship.
+                                    <br /><br />
+                                    I am currently pursuing a Bachelor’s degree in Computer Science at
+                                    <Link target="_blank" href='https://ufam.edu.br/'> Federal University of Amazonas (UFAM)</Link>,
+                                    and I hold a technical education in Software Development from
+                                    <Link target="_blank" href='https://www.fundacaomatiasmachline.org.br/'> Fundação Matias Machline</Link>.
                                 </Paragraph>
+
                                 <Box align='center' my={4}>
                                     <NextLink href='/works'>
                                         <Button rightIcon={<ChevronRightIcon />}>
@@ -69,7 +87,7 @@ const Page = () => {
 
                             <Section delay={0.3}>
                                 <Heading as='h3' variant="section-title">
-                                    🛠️ Skills
+                                    🛠️ Skills & Tools
                                 </Heading>
                                 <Grid marginY='3rem' templateColumns="repeat(auto-fit, minmax(120px, 1fr))" gap={6}>
                                     {TECH_SKILLS_DATA.map((item) => (
