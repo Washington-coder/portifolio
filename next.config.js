c905850d9a13f44ee5994c702a1e908d79c2f744
Washington <was.antonio.xy@gmail.com>
2025-11-07 16:45:15 -0400

    chore: github pages config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-    reactStrictMode: true
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    basePath: '/portifolio',
+    assetPrefix: '/portifolio/',
 }
+
+module.exports = nextConfig
